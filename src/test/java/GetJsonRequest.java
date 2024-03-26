49e324c42713e3e6bf0a4612b01ee0ec8779c05d
Kirill Ivanenko <kv.ivanenko@mail.ru>
2024-03-26 19:20:55 +0300

    Get second Timestamp

---
+++ src/test/java/GetJsonRequest.java
@@ -0,0 +1,16 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+public class GetJsonRequest {
+    @Test
+    public void jsonParsingTest() {
+        JsonPath response = RestAssured
+                .given()
+                .when()
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+        System.out.println("All Timestamps: " + response.get("messages.timestamp"));
+        System.out.println("Second Timestamp : " + response.get("messages.timestamp[1]"));
+    }
+}
